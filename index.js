b3fef6d19eb0a0f51ead3e16fabd2beae805bb15
CHEVY S. HUNGERFORD <cshunger@us.ibm.com>
2017-05-12 17:38:19 -0500

    setting redhat hosts

--- index.js
+++ index.js
@@ -117,5 +117,10 @@ app.post('/subscribe', function(incomingReq, outGoingRes) {
 
 });
 
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
-app.listen(3000);
+
+http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
