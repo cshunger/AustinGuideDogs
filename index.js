a83a8de4adb4339827564f57b9b4f0e19a3b6275
CHEVY S. HUNGERFORD <cshunger@us.ibm.com>
2017-05-13 08:06:01 -0500

    adding ip to console log

--- index.js
+++ index.js
@@ -125,5 +125,5 @@ app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
 
 http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server ' + app.get('ip') + 'listening on port ' + app.get('port'));
 });
