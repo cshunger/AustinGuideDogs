0c780fc3587e684ae0caee1401819f71fd6fdde8
CHEVY S. HUNGERFORD <cshunger@us.ibm.com>
2017-05-13 08:16:04 -0500

    listening on ip 0.0.0.0

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.post('/subscribe', function(incomingReq, outGoingRes) {
 });
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
 
 
 http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
