7e22b1d4532ac31f048d5a2f4c3964bbd41be5c3
CHEVY S. HUNGERFORD <cshunger@us.ibm.com>
2017-11-06 20:24:56 -0600

    don't uglify js, debugging

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ var useref = require('gulp-useref');
 var cssnano = require('gulp-cssnano');
 var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
+var gp_concat = require('gulp-concat');
 var del = require('del');
 
 gulp.task('useref', function(){
     return gulp.src('app/*.html')
         .pipe(useref())
         // Minifies only if it's a JavaScript file
-        .pipe(gulpIf('*.js', uglify()))
         .pipe(gulpIf('*.css', cssnano()))
         .pipe(gulp.dest('dist'))
 })
@@ -22,8 +22,14 @@ gulp.task('images', function(){
   .pipe(gulp.dest('dist/images'))
 });
 
+gulp.task('js', function(){
+        return gulp.src(['./app/javascritp/form.js', './app/javascritp/getData.js', './app/javascritp/navbar.js'])
+            .pipe(gp_concat('concat.js'))
+            .pipe(gulp.dest('js'));
+    });
+
 gulp.task('clean:dist', function() {
   return del.sync('dist');
 })
 
-gulp.task('build', ['useref', 'images']);
+gulp.task('build', ['useref', 'images', 'js']);
